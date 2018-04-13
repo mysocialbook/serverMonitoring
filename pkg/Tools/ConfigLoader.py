1dcabdd202a07220c9a358236da15e9837ab6e92
David <david@likebook.me>
2018-04-13 14:03:45 +0200

    Fixed config path

--- pkg/Tools/ConfigLoader.py
+++ pkg/Tools/ConfigLoader.py
@@ -1,10 +1,11 @@
 import configparser
+import os
 
 
 class ConfigLoader:
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read('../config.ini')
+        self.config.read(os.path.dirname(os.path.realpath(__file__)) + '/../../config.ini')
 
     def get_section(self, section):
         if section in self.config:
