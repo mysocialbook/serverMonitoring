39c09dbd86479d273962cca90ef9cc29036dc6a8
chindit <littletiger58@gmail.com>
2019-03-13 09:51:24 +0100

    Fix issues with python3 on Ubuntu 18.04. Cli is crashing without explicit errors

--- serverTools/Tools/ConfigLoader.py
+++ serverTools/Tools/ConfigLoader.py
@@ -5,12 +5,13 @@ import os
 class ConfigLoader:
     def __init__(self, config):
         self.config = configparser.ConfigParser()
-        config_file = os.path.realpath(__file__) + '/../../config.ini'
+        config_file = os.getcwd() + '/../config.ini'
         if config:
             config_file = config
-        if not os.path.isfile(config):
+        if not os.path.isfile(config_file):
             raise RuntimeError('«'+config_file+'» is not a valid config file.  Please use full path')
-        self.config.read(config_file)
+        else:
+            self.config.read(config_file)
 
     def get_section(self, section):
         if section in self.config:
